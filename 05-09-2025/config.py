36bad85ae3b94d5c6bea81418bf24da726f15463
ovisha466 <ovishasanyal1@gmail.com>
2025-09-05 11:54:39 +0530

    Email sending done

---
+++ 05-09-2025/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Your Gmail address (the one you used to generate the app password)
+from_address = "ovishasanyal1@gmail.com"
+
+# Paste the 16-character app password from Google (no spaces)
+app_password = "ctic roaf vblw ixjy" 
+
+# Your sir's email address
+to_address = "gmaheswaranmca@gmail.com"
